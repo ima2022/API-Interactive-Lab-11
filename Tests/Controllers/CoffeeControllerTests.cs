2fafbd215283cfe6cdb03c3bddf584c7b182634b
Abuzaid <104914994+ima2022@users.noreply.github.com>
2022-07-01 02:29:07 +0100

    initial commit

---
+++ Tests/Controllers/CoffeeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using API_Interactive_Lab_1.Controllers;
+using NUnit.Framework;
+using API_Interactive_Lab_1.Models;
+using API_Interactive_Lab_1;
+namespace API_Interactive_Lab_1.Tests
+{
+    public class CoffeeControllerTests
+    {
+        [Test]
+        public void TestGetHome()
+        {
+
+            string expectedContent = "I Like Coffee!";
+
+            var controller = new CoffeeController();
+            var result = controller.GetCoffeeLover();
+            Assert.AreEqual(expectedContent, result);
+        }
+    }
+}
